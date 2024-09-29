8be811fe4c9c8342248590d74f9a9819b7774c8f
Nikita Umanskii <nikitaumanskiy1998@mail.ru>
2024-09-29 20:01:02 +0200

    changed to without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
+
+// export const App = () => {
+// 	// Императивный код: Мы создаём экземпляр объекта `Date` и явно получаем текущий год
+// 	const currentYear = new Date().getFullYear();
+
+// 	return (
+// 		// Декларативный код: Мы описываем, как должен выглядеть интерфейс
+// 		<div className="App">
+// 			<header className="App-header">
+// 				{/* Декларативный код: Описание изображения и его стиля */}
+// 				<img src={logo} className="App-logo" alt="logo" />
+
+// 				{/* Декларативный код: Описание текста и его стиля */}
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+
+// 				{/* Декларативный код*/}
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+
+// 				{/* Декларативный код */}
+// 				<p>{currentYear}</p>
+// 			</header>
+// 		</div>
+// 	);
+// };
 
 export const App = () => {
 	// Императивный код: Мы создаём экземпляр объекта `Date` и явно получаем текущий год
 	const currentYear = new Date().getFullYear();
 
-	return (
+	return createElement(
 		// Декларативный код: Мы описываем, как должен выглядеть интерфейс
-		<div className="App">
-			<header className="App-header">
-				{/* Декларативный код: Описание изображения и его стиля */}
-				<img src={logo} className="App-logo" alt="logo" />
-
-				{/* Декларативный код: Описание текста и его стиля */}
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-
-				{/* Декларативный код*/}
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-
-				{/* Декларативный код */}
-				<p>{currentYear}</p>
-			</header>
-		</div>
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement(
+				/* Декларативный код: Описание изображения и его стиля */
+				'img',
+				{
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				},
+			),
+			/* Декларативный код: Описание текста и его стиля */
+			createElement('p', null, 'Edit <code>src/App.js</code> and save to reload.'),
+			createElement(
+				/* Декларативный код*/
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement(
+				/* Декларативный код */
+				'p',
+				null,
+				currentYear,
+			),
+		),
 	);
 };
