9378522c3fa0df47d9b1c682a37f629306305b83
Nikita Umanskii <nikitaumanskiy1998@mail.ru>
2024-09-29 15:19:15 +0200

    created first project-React with JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
+	// Императивный код: Мы создаём экземпляр объекта `Date` и явно получаем текущий год
+	const currentYear = new Date().getFullYear();
+
 	return (
+		// Декларативный код: Мы описываем, как должен выглядеть интерфейс
 		<div className="App">
 			<header className="App-header">
+				{/* Декларативный код: Описание изображения и его стиля */}
 				<img src={logo} className="App-logo" alt="logo" />
+
+				{/* Декларативный код: Описание текста и его стиля */}
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
+
+				{/* Декларативный код*/}
 				<a
 					className="App-link"
 					href="https://reactjs.org"
@@ -17,6 +26,9 @@ export const App = () => {
 				>
 					Learn React
 				</a>
+
+				{/* Декларативный код */}
+				<p>{currentYear}</p>
 			</header>
 		</div>
 	);
